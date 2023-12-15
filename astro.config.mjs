9107d03fd337ddfcb64282fb7124346f496a4906
Angel Gabriel Lopez <angel178293lopez@gmail.com>
2023-12-14 21:08:39 -0400

    Fix import statements and update sidebar links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,12 +9,14 @@ export default defineConfig({
       title: 'GNX - Utilities',
       social: {
         github: 'https://github.com/ImRLopezAG/generic-service',
-        twitter: 'https://twitter.com/ImR_Lopez',
+        twitter: 'https://twitter.com/ImR_Lopez'
       },
+      favicon: '/public/favicon.svg',
+      
       sidebar: [
         {
           label: 'Getting Started',
-          link: '/packages/getting-started',
+          link: '/packages/getting-started'
         },
         {
           label: 'Packages',
@@ -26,36 +28,52 @@ export default defineConfig({
               link: '/packages/decorators',
               badge: {
                 text: 'new',
-                variant: 'success',
-              },
+                variant: 'success'
+              }
             },
-          ],
+            {
+              label: 'Validators',
+              link: '/global/soon',
+              badge: {
+                text: 'Coming Soon',
+                variant: 'tip'
+              }
+            }
+          ]
         },
         {
           label: 'API',
           items: [
             {
               label: 'Models',
-              link: '/api/models',
+              link: '/api/models'
             },
             {
               label: 'Services',
-              link: '/api/services',
+              link: '/api/services'
             },
             {
               label: 'Decorators',
               link: '/api/decorators',
               badge: {
                 text: 'new',
-                variant: 'success',
-              },
+                variant: 'success'
+              }
             },
-          ],
-        },
+            {
+              label: 'Validators',
+              link: '/global/soon',
+              badge: {
+                text: 'Coming Soon',
+                variant: 'tip'
+              }
+            }
+          ]
+        }
       ],
       customCss: ['./src/tailwind.css'],
-      expressiveCode: true,
+      expressiveCode: true
     }),
-    tailwind({ applyBaseStyles: false }),
-  ],
-});
+    tailwind({ applyBaseStyles: false })
+  ]
+})
